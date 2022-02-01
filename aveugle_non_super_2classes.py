425852bf20dc9d5689175a096b74153b40e1458a
Koowah <anaslargab@gmail.com>
2022-02-01 05:02:16 +0100

    commit on 'main'

--- aveugle_non_super_2classes.py
+++ aveugle_non_super_2classes.py
@@ -33,6 +33,9 @@ def calc_posterior_Gauss(Y, m, n, cl1, cl2, p1, p2, m1, sig1, m2, sig2):
     
     return post_cl1, post_cl2
 
+def posterior_sampling(post_cl1, post_cl2, cl1, cl2, m, n): # tirage_apost
+    pass
+
 def est_empirical(X, Y, cl1, cl2):
     p1 = (X == cl1).sum() / np.prod(X.shape)
     p2 = (X == cl2).sum() / np.prod(X.shape)
@@ -85,7 +88,7 @@ def calc_EM(Y, m, n, cl1, cl2, p10, p20, m10, sig10, m20, sig20, nb_iterEM):
 def main():
     path = './images_BW/cible2.bmp'
     X, shape = read_image(path)
-    display_image('X (press any key to close window)', X * 255) # displays X
+    display_image('X', X * 255) # displays X
     
     cl1, cl2 = identify_classes(X) # identify image classes (two in our case)
     print(f'Class 1 : {cl1} (black)\nClass 2 : {cl2} (white)') 
@@ -97,22 +100,22 @@ def main():
     
     m1, m2, sig1, sig2 = .2, 0, .3, .3 # gaussian noise parameters    
     Y = X + gauss_noise(X, m, n, cl1, cl2, m1, sig1, m2, sig2) # adding noise to image
-    display_image('Y (press any key to close window)', Y)
+    display_image('Y', Y)
     
-    # Hocus pocus, you lost your focus ! and forgot everything you knew about the above parameters
+    ################################################################################################
+    # Hocus pocus, you lost your focus ! and forgot everything you knew about the above parameters #
+    ################################################################################################
+    
     p1, p2, m1, m2, sig1, sig2 = 0, 0, 0, 0, 0, 0
     
-    p1, p2, m1, sig1, m2, sig2 = est_empirical(init_param(Y, cl1, cl2), Y, cl1, cl2)
-    p1_em, p2_em, m1_em, sig1_em, m2_em, sig2_em, dic = calc_EM(Y, m, n, cl1, cl2, p1, p2, m1, sig1, m2, sig2, 300)
+    p1, p2, m1, sig1, m2, sig2 = est_empirical(init_param(Y, cl1, cl2), Y, cl1, cl2) # empirically estimating a starting point for EM algorithm
+    p1_em, p2_em, m1_em, sig1_em, m2_em, sig2_em, dic = calc_EM(Y, m, n, cl1, cl2, p1, p2, m1, sig1, m2, sig2, 300) # EM algorithm estimates
+    
     print(f'p1: {p1_em}, p2 : {p2_em}, m1 : {m1_em}, sig1 : {sig1_em}, m2 : {m2_em}, sig2 : {sig2_em}')
     
     df = pd.DataFrame.from_dict(dic)
-    
     sns.set()
-    sns.relplot(data=df, kind='line')
-    # uncomment lines below to plot each variable separately
-    # for column in df.columns:
-    #     sns.relplot(data=df[column], kind='line', palette='crest')
+    sns.relplot(data=df, kind='line') # Plotting evolution of parameters over EM iterations
     plt.show()
 
 # si le bruit 0, 0, .6, .6 ça converge vers un minimum local        
