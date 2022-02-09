8c49bbee5a97a51108b61ed2132ff9f2b71f7f57
Koowah <anaslargab@gmail.com>
2022-02-09 14:18:36 +0100

    clean up code split em sem

--- aveugle_non_super_2classes.py
+++ aveugle_non_super_2classes.py
@@ -19,30 +19,29 @@ def calc_posterior_Gauss(Y, m, n, cl1, cl2, p1, p2, m1, sig1, m2, sig2):
     # posterior = p(X | Y) = p(Y|X)p(X) / sum(p(Y|X)p(X)) for each class X
     flat_Y = Y.reshape(-1,1)
 
-    if (p1, p2, m1, sig1, m2, sig2) == (0, 0, 0, 0, 0, 0):
-        post_cl1 = np.invert(flat_Y)
-        post_cl2 = flat_Y
-    else:
-        norm_1 = norm(m1, sig1)
-        norm_2 = norm(m2, sig2)
-        
-        f1 = p1*norm_1.pdf(flat_Y - cl1) # this way we call stats.norm only once
-        f2 = p2*norm_2.pdf(flat_Y - cl2)
-        
-        post_cl1 = (f1 / (f1 + f2))
-        post_cl2 = (f2 / (f1 + f2))
-        
-        # posterior = np.concatenate((post_cl1, post_cl2))
-        # assert np.prod(posterior.shape) == m*n*2, 'posterior shape should be m*n*2'
+    # if (p1, p2, m1, sig1, m2, sig2) == (0, 0, 0, 0, 0, 0):
+    #     post_cl1 = 1 - flat_Y
+    #     post_cl2 = flat_Y
+    # else:
+    norm_1 = norm(m1, sig1)
+    norm_2 = norm(m2, sig2)
     
+    f1 = p1*norm_1.pdf(flat_Y - cl1) # this way we call stats.norm only once
+    f2 = p2*norm_2.pdf(flat_Y - cl2)
+    
+    post_cl1 = (f1 / (f1 + f2))
+    post_cl2 = (f2 / (f1 + f2))
+    
+    # posterior = np.concatenate((post_cl1, post_cl2))
+    # assert np.prod(posterior.shape) == m*n*2, 'posterior shape should be m*n*2'
     
     return post_cl1, post_cl2
 
 def posterior_sampling(post_cl1, post_cl2, cl1, cl2, m, n): # tirage_apost
     sample = np.random.rand(m*n).reshape(-1,1) # m*n samples from uniform distribution over [0,1)
     
-    condition1 = sample < post_cl1 # we draw cl1 with posterior probability of cl1 aka when our uniform sample(i) < post_cl1(i) and cl2 otherwise
-    condition2 = sample < post_cl2
+    condition1 = sample <= post_cl1 # we draw cl1 with posterior probability of cl1 aka when our uniform sample(i) < post_cl1(i) and cl2 otherwise
+    condition2 = sample <= post_cl2
     X_est = (cl1 * condition1) + (cl2 * condition2)
     
     return X_est
@@ -123,8 +122,8 @@ def calc_EM(Y, m, n, cl1, cl2, p10, p20, m10, sig10, m20, sig20, nb_iterEM):
 ###################################################################################
 
 
-def main():
-    path = './images_BW/cible2.bmp'
+def main(em=True, sem=True):
+    path = './images_BW/country2.bmp'
     X, shape = read_image(path)
     display_image('X', X * 255) # displays X
     
@@ -134,9 +133,9 @@ def main():
     m, n = shape[0], shape[1] # X shape
     
     p1, p2 = calc_prior(X, m, n, cl1, cl2) # priors of each class
-    print(f'\np(cl1) = p1 = {p1}\np(cl2) = p2 = {p2}')
+    print(f'\np(cl1) = p1 = {p1:.3f}\np(cl2) = p2 = {p2:.3f}')
     
-    m1, sig1, m2, sig2 = .2, .6, -.3, .6 # gaussian noise parameters    
+    m1, sig1, m2, sig2 = .3, .1, -.4, .1 # gaussian noise parameters    
     Y = X + gauss_noise(X, m, n, cl1, cl2, m1, sig1, m2, sig2) # adding noise to image
     print(f'm1 = {m1}, sig1 = {sig1}, m2 = {m2}, sig2 = {sig2}')
     display_image('Y', Y)
@@ -145,22 +144,32 @@ def main():
     # Hocus pocus, you lost your focus ! and forgot everything you knew about the above parameters #
     ################################################################################################
     
-    p1, p2, m1, m2, sig1, sig2 = 0, 0, 0, 0, 0, 0 # he 4gett ... but he also estimett !
-    p1, p2, m1, sig1, m2, sig2 = est_empirical(init_param(Y, cl1, cl2), Y, cl1, cl2) # empirically estimating a starting point for EM & SEM algorithms
-    
-    p1_sem, p2_sem, m1_sem, sig1_sem, m2_sem, sig2_sem, dic_sem = calc_SEM(Y, m, n, cl1, cl2, p1, p2, m1, sig1, m2, sig2, 300)
-    p1_em, p2_em, m1_em, sig1_em, m2_em, sig2_em, dic_em = calc_EM(Y, m, n, cl1, cl2, p1, p2, m1, sig1, m2, sig2, 300) # EM algorithm estimates
-    
-    print(f'\nEM\np1: {p1_em:.3f}, p2 : {p2_em:.3f}, m1 : {m1_em:.3f}, sig1 : {sig1_em:.3f}, m2 : {m2_em:.3f}, sig2 : {sig2_em:.3f}')
-    print(f'SEM\np1: {p1_sem:.3f}, p2 : {p2_sem:.3f}, m1 : {m1_sem:.3f}, sig1 : {sig1_sem:.3f}, m2 : {m2_sem:.3f}, sig2 : {sig2_sem:.3f}')
-    
-    df_em = pd.DataFrame.from_dict(dic_em)
-    df_sem = pd.DataFrame.from_dict(dic_sem)
-    
+    # p1, p2, m1, m2, sig1, sig2 = 0, 0, 0, 0, 0, 0 # he 4gett ... but he also estimett !
+    # p1, p2, m1, sig1, m2, sig2 = est_empirical(init_param(Y, cl1, cl2), Y, cl1, cl2) # empirically estimating a starting point for EM & SEM algorithms
+
     sns.set()
-    sns.relplot(data=df_em, kind='line') # Plotting evolution of parameters over EM iterations
-    sns.relplot(data=df_sem, kind='line') # Plotting evolution of parameters over SEM iterations
-    plt.show()
+    
+    if em:
+        p1_em, p2_em, m1_em, sig1_em, m2_em, sig2_em, dic_em = calc_EM(Y, m, n, cl1, cl2, p1, p2, m1, sig1, m2, sig2, 100) # EM algorithm estimates
+        error_em = ((abs(p1_em - p1)/p1) + (abs(p2_em - p2)/p2) + (abs(m1_em - m1)/m1) + (abs(sig1_em - sig1)/sig1) + (abs(m2_em - m2)/m2) + (abs(sig2_em - sig2)/sig2))/6 # mean error over parameters error rates
+        
+        print(f'\nEM\np1: {p1_em:.3f}, p2 : {p2_em:.3f}, m1 : {m1_em:.3f}, sig1 : {sig1_em:.3f}, m2 : {m2_em:.3f}, sig2 : {sig2_em:.3f}')
+        print(f'Error rate EM : {error_em:.3f}')
+        
+        df_em = pd.DataFrame.from_dict(dic_em)
+        sns.relplot(data=df_em, kind='line') # Plotting evolution of parameters over EM iterations
+   
+    if sem:
+        p1_sem, p2_sem, m1_sem, sig1_sem, m2_sem, sig2_sem, dic_sem = calc_SEM(Y, m, n, cl1, cl2, p1, p2, m1, sig1, m2, sig2, 100)
+        error_sem = ((abs(p1_sem - p1)/p1) + (abs(p2_sem - p2)/p2) + (abs(m1_sem - m1)/m1) + (abs(sig1_sem - sig1)/sig1) + (abs(m2_sem - m2)/m2) + (abs(sig2_sem - sig2)/sig2))/6
+        
+        print(f'SEM\np1: {p1_sem:.3f}, p2 : {p2_sem:.3f}, m1 : {m1_sem:.3f}, sig1 : {sig1_sem:.3f}, m2 : {m2_sem:.3f}, sig2 : {sig2_sem:.3f}')
+        print(f'Error rate SEM : {error_sem:.3f}')
+        
+        df_sem = pd.DataFrame.from_dict(dic_sem)
+        sns.relplot(data=df_sem, kind='line') # Plotting evolution of parameters over SEM iterations
+    
+    # plt.show()
 
 if __name__ == '__main__':
     # si le bruit 0, 0, .6, .6 ça converge vers un minimum local        
